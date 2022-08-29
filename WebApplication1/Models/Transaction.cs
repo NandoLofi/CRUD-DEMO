c9d9813022e1e26ef6efb8f94c928e19d8bcdff8
Fernando Galvan <n.brighter00@gmail.com>
2022-08-29 09:12:39 -0500

    adding new commits

---
+++ WebApplication1/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        public string AccountNumber { get; set; }
+
+        public string AccountHolder {  get; set; }
+
+        public string BankName { get; set; }
+
+        public string SWIFTCode { get; set; }
+
+        public int Amount { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
